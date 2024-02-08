6af919879eed2555ab1ca64e015a19a98859e8a4
Angel Gabriel Maldonado Salinas <gab.salinas15@gmail.com>
2024-02-08 17:51:42 -0600

    Register y login

--- testnext/components/Login.tsx
+++ testnext/components/Login.tsx
@@ -7,7 +7,6 @@ const Login = ({ onClose }: any) => {
     
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-
     const handleLogin = () => {
         console.log(`Login con correo: ${email}, y contrasena: ${password}`);
     }
--- testnext/components/Registro.tsx
+++ testnext/components/Registro.tsx
@@ -4,8 +4,7 @@ import Link from "next/link"
 import formStyles from "@/styles/formStyles.module.css"
 
 const Register = ({ onClose }: any) => {
-
-    
+  
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const handleRegister = () => {
